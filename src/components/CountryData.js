5329ba1590874d4799060270287b40a39755c330
Ivan Lytovka <ivanlytovka@gmail.com>
2019-08-22 14:17:24 -0400

    Integrate Mapbox to the project

--- src/components/CountryData.js
+++ src/components/CountryData.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactMapGl from 'react-map-gl';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
@@ -15,10 +16,24 @@ const CountryData = ({ country, store }) => {
 
     const classes = useStyles()
     console.log(country);
+
+    const mapObj = {
+        latitude: country.latlng[0],
+        longitude: country.latlng[1],
+        width: "auto",
+        height: "30vh",
+        zoom: 3
+    };
+
     return (
         <>
             <CssBaseline />
             <Container maxWidth="md">
+                <ReactMapGl
+                    {...mapObj}
+                    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
+                >
+                </ReactMapGl>
                 <div className={classes.root}>
                     <p>Name: {country.name} </p>
                     <p>Capital: {country.capital}</p>
